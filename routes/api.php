f3dd9565a36769319e9aefb6390b2dd1e75e6fd9
Antoine Buchser <antoine.buchser@epitech.eu>
2017-04-19 16:13:46 -0700

    fix to routes

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::group(['middleware' => ['auth:api']], function()
 	 */
 	Route::group(['prefix' => '/users'], function()
 	{
-		Route::put('/change-password', ['as' => 'users.change-password', 'uses' => 'UsersController@changePassword']);
+		Route::put('/change-password', ['as' => 'users.change-password', 'uses' => 'UserController@changePassword']);
 		Route::get('/profile/access-rules', ['as' => 'users.profile.access-rules', 'uses' => 'UserController@profileAccessRules']);
 		Route::get('/profile', ['as' => 'users.profile', 'uses' => 'UserController@profile']);
 	});
