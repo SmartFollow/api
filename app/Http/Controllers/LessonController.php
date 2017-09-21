5f61fcb4187575be6041423157e4d473f88d019d
Antoine Buchser <antoine.buchser@epitech.eu>
2017-09-21 16:04:58 +0200

    c dur langlé lol xd

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -68,7 +68,7 @@ class LessonController extends Controller
 			'MONDAY' => 1,
 			'TUESDAY' => 2,
 			'WEDNESDAY' => 3,
-			'THIRSDAY' => 4,
+			'THURSDAY' => 4,
 			'FRIDAY' => 5,
 			'SATURDAY' => 6,
 		];
