4a1675c89b854f62d8a0ba0de2efb1817b65eb56
Antoine Buchser <antoine.buchser@epitech.eu>
2017-11-08 21:58:22 +0100

    Loading group and studentClass info in users.index

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,7 +24,9 @@ class UserController extends Controller
     {
 		$this->authorize('index', User::class);
 
-		return User::get();
+		return User::with('studentClass')
+				   ->with('group')
+				   ->get();
     }
 
 	/**
