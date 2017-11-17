9e52c851bdcda08d16813389e3ffec7639a541c3
Antoine Buchser <antoine.buchser@epitech.eu>
2017-11-17 19:56:27 +0100

    Changes to notifications

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -10,167 +11,179 @@ use App\Models\Pedagogy\StudentClass;
 
 class NotificationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request)
-    {
-		$notifications = Notification::whereHas('users', function ($q) use ($request) {
-			$q->where('users.id', Auth::id());
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index(Request $request)
+	{
+		$notifications = Notification::with('transmitter')
+			->whereHas('users', function ($q) use ($request) {
+				$q->where('users.id', Auth::id());
 
-			if ($request->has('type') && $request->get('type') == 'unread')
-				$q->where('read_at', null);
-		});
+				if ($request->has('type') && $request->get('type') == 'unread')
+					$q->where('read_at', null);
+			});
 
 		$notifications = $notifications->get();
 
-        return $notifications;
-    }
+		return $notifications;
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
 		return [
 			'groups' => Group::get(),
 			'studentClasses' => StudentClass::get(),
 		];
-    }
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $this->validate($request, [
-            'resource_link' => '',
-            'message' => 'required',
-            'user' => 'required_without_all:users,group,student_class|exists:users,id',
-            'users.*' => 'required_without_all:user,group,student_class|exists:users,id',
-            'group' => 'required_without_all:user,users,student_class|exists:groups,id',
-            'student_class' => 'required_without_all:user,users,group|exists:student_classes,id',
-        ]);
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		$this->validate($request, [
+			'resource_link' => '',
+			'message' => 'required',
+			'user' => 'required_without_all:users,group,student_class|exists:users,id',
+			'users.*' => 'required_without_all:user,group,student_class|exists:users,id',
+			'group' => 'required_without_all:user,users,student_class|exists:groups,id',
+			'student_class' => 'required_without_all:user,users,group|exists:student_classes,id',
+		]);
 
-        $notification = new Notification();
-        $notification->transmitter_id = Auth::id();
-        $notification->resource_link = $request->get('resource_link');
-        $notification->message = $request->get('message');
-        $notification->save();
+		$notification = new Notification();
+		$notification->transmitter_id = Auth::id();
+		$notification->resource_link = $request->get('resource_link');
+		$notification->message = $request->get('message');
+		$notification->save();
 
-		if ($request->has('user'))
-		{
+		if ($request->has('user')) {
 			$notification->users()->attach($request->get('user'));
 		}
-		if ($request->has('users'))
-		{
+		if ($request->has('users')) {
 			$notification->users()->syncWithoutDetaching($request->get('users'));
 		}
-		if ($request->has('group'))
-		{
+		if ($request->has('group')) {
 			$group = Group::with('users')->findOrFail($request->get('group'));
 			$notification->users()->syncWithoutDetaching($group->users);
 		}
-		if ($request->has('student_class'))
-		{
+		if ($request->has('student_class')) {
 			$studentClass = StudentClass::with('students')->findOrFail($request->get('student_class'));
 			$notification->users()->syncWithoutDetaching($studentClass->students);
 		}
 
-        return $notification;
-    }
+		return $notification;
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $notification = Notification::findOrFail($id);
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		$notification = Notification::with('transmitter')
+									->with('users')
+									->findOrFail($id);
 
-        return $notification;
-    }
+		return $notification;
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $notification = Notification::findOrFail($id);
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($id)
+	{
+		$notification = Notification::findOrFail($id);
 
-        return $notification;
-    }
+		return $notification;
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $notification = Notification::findOrFail($id);
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @param  int $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		$notification = Notification::findOrFail($id);
 
-        $this->validate($request, [
-            'transmitter_id' => '',
-            'resource_link' => '',
-            'message' => '',
-            'user' => 'exists:users,id',
-        ]);
+		$this->validate($request, [
+			'transmitter_id' => '',
+			'resource_link' => '',
+			'message' => '',
+			'user' => 'required_without_all:users,group,student_class|exists:users,id',
+			'users.*' => 'required_without_all:user,group,student_class|exists:users,id',
+			'group' => 'required_without_all:user,users,student_class|exists:groups,id',
+			'student_class' => 'required_without_all:user,users,group|exists:student_classes,id',
+		]);
 
-        if ($request->has('transmitter_id'))
-            $notification->transmitter_id = Auth::id();
-        if ($request->has('resource_link'))
-            $notification->resource_link = $request->get('resource_link');
-        if ($request->has('message'))
-            $notification->message = $request->get('message');
-        $notification->save();
+		if ($request->has('transmitter_id'))
+			$notification->transmitter_id = Auth::id();
+		if ($request->has('resource_link'))
+			$notification->resource_link = $request->get('resource_link');
+		if ($request->has('message'))
+			$notification->message = $request->get('message');
+		$notification->save();
 
-        if ($request->has('user'))
-        {
-            $notification->users()->attach($request->get('user'));
-        }
+		if ($request->has('user')) {
+			$notification->users()->attach($request->get('user'));
+		}
+		if ($request->has('users')) {
+			$notification->users()->syncWithoutDetaching($request->get('users'));
+		}
+		if ($request->has('group')) {
+			$group = Group::with('users')->findOrFail($request->get('group'));
+			$notification->users()->syncWithoutDetaching($group->users);
+		}
+		if ($request->has('student_class')) {
+			$studentClass = StudentClass::with('students')->findOrFail($request->get('student_class'));
+			$notification->users()->syncWithoutDetaching($studentClass->students);
+		}
 
-        return $notification;
-    }
+		return $notification;
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $notification = Notification::findOrFail($id);
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		$notification = Notification::findOrFail($id);
 
-        $notification->delete();
-    }
+		$notification->delete();
+	}
 
-    /**
-     * Mark the notification as read.
-     *
-     * @param  int  $id
-     * @return void
-     */
-    public function markAsRead($id)
-    {
-        $notification = Notification::findOrFail($id);
+	/**
+	 * Mark the notification as read.
+	 *
+	 * @param  int $id
+	 * @return void
+	 */
+	public function markAsRead($id)
+	{
+		$notification = Notification::findOrFail($id);
 
 		$notification->users()->updateExistingPivot(Auth::id(), ['read_at' => new \DateTime()]);
-    }
+	}
 
 }
--- app/Models/Communication/Notification.php
+++ app/Models/Communication/Notification.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
+	protected $appends = ['users_count'];
+
     public function users()
 	{
 		return $this->belongsToMany('App\Models\Users\User')->withPivot('read_at');
@@ -15,5 +17,18 @@ class Notification extends Model
 	public function transmitter()
 	{
 		return $this->belongsTo(User::class, 'transmitter_id');
+	}
+
+	public function getUsersCountAttribute()
+	{
+		if (!array_key_exists('users', $this->relations)) {
+			$this->load('users');
+			$count = count($this->users);
+			unset($this->users);
+
+			return $count;
+		}
+		else
+			return count($this->users);
 	}
 }
